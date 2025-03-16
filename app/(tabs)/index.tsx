c1408ac97284921d67f576a2102cafd611e756c6
heyymasud <heyymasud@gmail.com>
2025-03-17 04:57:37 +0700

    feat: add home ui and searchbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,30 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Link, router } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl font-bold color-blue-400">Welcome Bro.</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: 10,
+          minHeight: "100%"
+        }} >
+        <Image
+          source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <View className="flex-1 mt-5">
+          <SearchBar 
+          placeholder="Search for a movie"
+          onPress={() => router.push("/search")}/>
+        </View>
+      </ScrollView>
     </View>
   );
 }
